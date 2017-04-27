4169c94a24657902cb710e74a818c8fe3c04ddf0
HonzoBonzo <konradb12@gmail.com>
2017-04-27 22:02:32 +0200

    added md-select for choosing nurse

---
+++ app/src/NurseScheduleController.js
@@ -0,0 +1,43 @@
+var app = angular.module('myApp')
+app.controller('NurseScheduleController', function($scope, $rootScope) {
+  var vm = this;
+  vm.showMe = false;
+  vm.nurses = [
+    {
+      id: 0,
+      name: 'Basia'
+    },
+    {
+      id: 1,
+      name: 'Stasia'
+    },
+    {
+      id: 2,
+      name: 'Kasia'
+    },
+    {
+      id: 3,
+      name: 'Ola'
+    }
+  ];
+
+  vm.chosenNurse;
+
+  vm.changeNurse = changeNurse;
+
+  function changeNurse() {
+    console.log(vm.chosenNurse);
+  }
+
+  vm.getSelectedText = function() {
+    if (vm.chosenNurse !== undefined) {
+      return "You have selected: " + vm.chosenNurse.name;
+    } else {
+      return "Please select a nurse";
+    }
+  };
+
+  $rootScope.$on("nurseSchedule", function() {
+    vm.showMe = true;
+  })
+})
