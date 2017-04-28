e5bca81ebbf0aec38d4010499ee08b17896bd32c
HonzoBonzo <konradb12@gmail.com>
2017-04-28 10:51:32 +0200

    added simple schedule

--- app/src/NurseScheduleController.js
+++ app/src/NurseScheduleController.js
@@ -38,6 +38,32 @@ app.controller('NurseScheduleController', function($scope, $rootScope) {
     }
   };
 
+  vm.checkBreak = function(index) {
+    if(index % 7 === 0) {
+      return true;
+    }
+    return false;
+  }
+
+  vm.getDayName = function(id) {
+    switch(id % 7){
+      case 0: 
+			  return 'Monday'
+      case 1: 
+        return 'Tuesday'
+      case 2: 
+        return 'Wednesday'
+      case 3: 
+        return 'Thursday'
+      case 4:
+        return 'Friday'
+      case 5:
+        return 'Saturday'
+      case 6:
+        return 'Sunday'
+    }
+  }
+
   $rootScope.$on("hide", function() {
     vm.showMe = false;
   })
