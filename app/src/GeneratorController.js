d788131d59294871554809b176883d7cd04bb2b6
HonzoBonzo <konradb12@gmail.com>
2017-06-03 16:53:16 +0200

    ui for import first week

--- app/src/GeneratorController.js
+++ app/src/GeneratorController.js
@@ -3,20 +3,42 @@ app.controller('GeneratorController', function($timeout, $scope, $rootScope, Ser
   var vm = this;
   vm.showMe = false;
   vm.msg = {};
+  vm.fileAddedFlag = false;
+  vm.msgInfo = '';
+  vm.loader = false;
 
   vm.generateSchedule = function() {
+    vm.loader = true;
     var promise = ServerService.generateSchedule();
     promise.then(data => {
       vm.msg = data;
       console.log(vm.msg)
       if(vm.msg.code === 200) {
         $timeout(function() {
+          vm.loader = false; 
           angular.element(
             document.querySelector('#fullSchedule')
           ).triggerHandler('click');
-        }, 0);
+        }, 2000);
       }
     });
+  }
+
+  vm.addFirstWeek = function() {
+    vm.msgInfo = 'Please choose file';
+    vm.fileAddedFlag = false;
+    var f = document.getElementById('file-4').files[0];
+    var r = new FileReader();
+
+    r.onloadend = function(e) {
+      var data = e.target.result;
+      vm.fileAddedFlag = true;
+      vm.msgInfo = 'You chose file: '+ f.name;
+      $scope.$apply();
+      //send your binary data via $http or $resource or do anything else with it
+    }
+
+    r.readAsBinaryString(f);
   }
   
   $rootScope.$on("hide", function() {
